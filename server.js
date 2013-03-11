e4850e094321567a2cfc6e5eb54e999ff7c6bfbc
CristianV <Cristia@hlmbusiness.com>
2013-03-11 11:15:12 -0500

    Adding package managment and fixing server.js

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-var wapp = require("./cirrus.js")
+var wApp = require("./cirrus.js")
 
-wapp.userscontroller = {
+wApp.usersController = {
   show: function(params){
-    return({hello: "world", id: wapp.router.params.userid, x: wapp.router.params.x})}
+    return({hello: "world", id: wApp.router.params.userid, x: wApp.router.params.x})}
 }
-wapp.router.addroutes({"get /users/:userid/show": "userscontroller#show"});
+wApp.router.addRoutes({"GET /users/:userid/show": "usersController#show"});
 
 
 // load the net module to create a tcp server.
 var net = require('net');
 
 // setup a tcp server
-var server = net.createserver(function (socket) {
+var server = net.createServer(function (socket) {
   var request = ""
   // every time someone connects, tell them hello and then close the connection.
-  socket.addlistener("connect", function () {
+  socket.addListener("connect", function () {
     socket.on('data', function(data) {
-        request = wapp.request(data.tostring())
-        socket.end(wapp.response(request));
+        console.log(data.toString())
+        request = wApp.request(data.toString())
+        socket.end(wApp.response(request));
     })
   });
 
 });
 
 // fire up the server bound to port 7000 on localhost
-server.listen(4000, "localhost");
+server.listen(5000, "localhost");
 console.log("tcp server listening on port 5000 at localhost.");
