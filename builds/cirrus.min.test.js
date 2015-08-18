09df7d00f8b41448b6a812c47a0ccd4b6c64b021
CristianV <cristian@kommit.co>
2015-08-18 09:08:03 -0500

    Fixing issue with body params and json header

--- builds/cirrus.min.test.js
+++ builds/cirrus.min.test.js
@@ -407,12 +407,13 @@ c,d,e,f,g){e={helpers:e,partials:f,data:g};if(a===l)throw new b.Exception("The p
       try{
           var req = http_parser(http_request);
           wApp.router.params = req.decodeParams;
+          if (req.bodyDecoded !== undefined) { wApp.router.params.body = req.bodyDecoded; }
 
           // body json
-          if ( req.headers["Content-Type"] === "application/json" && req.body){ wApp.router.params.body = JSON.parse(req.body); }
+          if ( req.headers["Content-Type"] !== undefined && req.headers["Content-Type"].match(/application\/json/i) && req.body){ wApp.router.params.body = JSON.parse(req.body); }
 
           // body XML
-          else if ( req.headers["Content-Type"] === "text/xml" && req.body){ wApp.router.params.body = req.body; }
+          else if ( req.headers["Content-Type"] !== undefined && req.headers["Content-Type"].match(/text\/xml/i) && req.body){ wApp.router.params.body = req.body; }
 
           else{ wApp.router.params.body = req.bodyDecoded; }
 
