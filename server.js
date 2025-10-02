d969b2e14c36761d8312061870001e4b57e641dc
cristian <Heavyblade@users.noreply.github.com>
2025-10-02 15:31:39 -0500

    Updating server with a better port

--- server.js
+++ server.js
@@ -9,17 +9,19 @@ wApp.usersController = {
 };
 
 wApp.router.addRoutes({"GET /users/:userid/show": "usersController#show"});
+wApp.router.addRoutes({"POST /users/:userid/show": "usersController#show"});
 // xxxxxxxxxxxxxxxx setup a TCP server xxxxxxxxxxxxxxxxxxxxxxx
 
-var HOST = '0.0.0.0';
-var PORT = 5000;
+var HOST = 'localhost';
+var PORT = 4001;
 
 net.createServer(function(sock) {
     sock.on('data', function(data) {
         wApp.router.params = {};
         request = wApp.request(data.toString());
+        console.log(request);
         sock.write(wApp.response(request));
     });
-}).listen(process.env.PORT || 5000);
+}).listen(process.env.PORT || 4001);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
